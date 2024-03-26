1b19b8de17508dae28a9f0a701781577ffd16fc0
Sabrina Zaki Hansen <87586469+sabszh@users.noreply.github.com>
2024-03-26 08:41:24 +0100

    streamlit up n runnin

---
+++ streamlit_app/streamlit.py
@@ -0,0 +1,37 @@
+from main import ChatBot
+import streamlit as st
+
+bot = ChatBot()
+    
+st.set_page_config(page_title="EER Chatbot")
+with st.sidebar:
+    st.title('EER Chatbot')
+
+# Function for generating LLM response
+def generate_response(input):
+    result = bot.rag_chain.invoke(input)
+    return result
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Hi, how can I help you today?"}]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# User-provided prompt
+if input := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": input})
+    with st.chat_message("user"):
+        st.write(input)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Generating an answer.."):
+            response = generate_response(input) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
