583c18e201bc57004289ce27ec74dac13a121af9
Sabrina Zaki Hansen <87586469+sabszh@users.noreply.github.com>
2024-06-13 10:53:59 +0200

    updates to prevent memory leaks

--- src/main.py
+++ src/main.py
@@ -8,63 +8,20 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint as HuggingFaceHub
 from dotenv import load_dotenv
-from pinecone import Pinecone as pc
-from pinecone import PodSpec
 import os
-
-from data_chunking import datachunk
 
 load_dotenv()
 
 class ChatBot():
-    
-    embeddings = HuggingFaceEmbeddings()
-    #pinecone_instance = pc(api_key=os.getenv('PINECONE_API_KEY'), embeddings=embeddings)
-    
-    index_name = "eerbot"
-    environment = "gcp-starter"
-    spec = PodSpec(environment=environment)
-    
-    """ if index_name not in pinecone_instance.list_indexes().names():
-        docs = datachunk()
-        pinecone_instance.create_index(name=index_name, metric="cosine", dimension=768, spec=spec)
-        docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
-        print("Created new Pinecone index and loaded documents")
-    else:
-        docsearch = Pinecone.from_existing_index(index_name, embeddings)
-        print("Using existing Pinecone index") """
-    
-    docsearch = Pinecone.from_existing_index(index_name, embeddings)
-    
-    llm = None  # Define llm, it will be initialized in __init__
+    def __init__(self, custom_template=None, repo_id=None, temperature=0.8):
+        self.embeddings = HuggingFaceEmbeddings()
+        self.index_name = "eerbot"
+        self.docsearch = Pinecone.from_existing_index(self.index_name, self.embeddings)
 
-    default_template = """
-    You are a chatbot working for the Experimenting Experiencing Reflecting (EER) Project, a research endeavor investigating the connections between art and science.
-    You have access to a collection of documents, including descriptions of research activities, meeting transcripts, and other relevant materials.
-    Your main task is to help the user explore and reflect on the EER project.
-    All questions should pertain to the EER Project unless specified otherwise.
-    When possible, please cite source documents in your answer (calling the documents the transcript date, e.g. "2021-05-28", or website page after the "/").
-    """
-    
-    template_end = """
-    Context: {context}
-    Question: {question}
-    Answer: 
-    
-    """
-    
-    def __init__(self, custom_template=None, repo_id=None, temperature=0.8, retriever_method='multiquery_retriever_llm'):
-        if custom_template:
-            self.template = custom_template
-        else:
-            self.template = self.default_template
-        
+        self.template = custom_template if custom_template else self.default_template()
         self.temperature = temperature
         self.repo_id = repo_id
-        self.retriever_method = retriever_method
-        docsearch = self.docsearch
         
-        # Initialize llm with repo_id
         self.llm = HuggingFaceHub(
             repo_id=self.repo_id,
             temperature=temperature,
@@ -73,19 +30,30 @@ class ChatBot():
             huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_KEY')
         )
 
-        if retriever_method == "docsearch":
-            retriever_method = docsearch.as_retriever()
-        elif retriever_method == "multiquery_retriever_llm":
-            multiquery_retriever_llm = MultiQueryRetriever.from_llm(retriever=docsearch.as_retriever(), llm=self.llm)
-            retriever_method = multiquery_retriever_llm
-
+        multiquery_retriever_llm = MultiQueryRetriever.from_llm(retriever=self.docsearch.as_retriever(), llm=self.llm)
 
         # Initialize the chain
         self.rag_chain = (
-            {"context": retriever_method, "question": RunnablePassthrough()}
-            | PromptTemplate(template=self.template+self.template_end, input_variables=["context", "question"])
+            {"context": multiquery_retriever_llm, "question": RunnablePassthrough()}
+            | PromptTemplate(template=self.template + self.template_end(), input_variables=["context", "question"])
             | self.llm
             | StrOutputParser()
         )
 
         print("Chain assembled...")
+
+    def default_template(self):
+        return """
+        You are a chatbot working for the Experimenting Experiencing Reflecting (EER) Project, a research endeavor investigating the connections between art and science.
+        You have access to a collection of documents, including descriptions of research activities, meeting transcripts, and other relevant materials.
+        Your main task is to help the user explore and reflect on the EER project.
+        All questions should pertain to the EER Project unless specified otherwise.
+        When possible, please cite source documents in your answer (calling the documents the transcript date, e.g. "2021-05-28", or website page after the "/").
+        """
+    
+    def template_end(self):
+        return """
+        Context: {context}
+        Question: {question}
+        Answer: 
+        """
--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,45 +2,44 @@ from main import ChatBot
 import streamlit as st
 
 repositories = {
-    "Mixtral-8x7B-Instruct-v0.1": "mistralai/Mixtral-8x7B-Instruct-v0.1",
     "Mistral-7B-Instruct-v0.2": "mistralai/Mistral-7B-Instruct-v0.2",
+    "Mixtral-8x7B-Instruct-v0.1": "mistralai/Mixtral-8x7B-Instruct-v0.1",
     "Mistral-7B-Instruct-v0.1": "mistralai/Mistral-7B-Instruct-v0.1"
 }
 
-retriever_methods = {
-    "MultiQueryRetriever": "multiquery_retriever_llm",
-    "Vector store-backed retriever": "docsearch"
-}
-
 st.set_page_config(page_title="EER Chatbot")
+
 with st.sidebar:
     st.title('EER Chatbot')
     st.write("""Be aware, if you make any changes here that the chatbot will reload and your chat will be gone.
              If you get an error, try a different model. If that does not work, it might be overloaded or down - so try again later.""")
     selected_repo = st.selectbox("Select the Model Repository", list(repositories.keys()))
-    selected_retriever = st.selectbox("Select the Retriever Method", list(retriever_methods.keys()))
     temperature = st.slider("Select the Temperature (0-2)", min_value=0.1, max_value=2.0, value=1.0, step=0.01)
     custom_prompt = st.text_area('Edit Preprompt',
     """You are a chatbot working for the Experimenting Experiencing Reflecting (EER) Project, a research endeavor investigating the connections between art and science.
     You have access to a collection of documents, including descriptions of research activities, meeting transcripts, and other relevant materials.
     Your main task is to help the user explore and reflect on the EER project.
     All questions should pertain to the EER Project unless specified otherwise.
-    When possible, please cite source documents in your answer (calling the documents the transcript date, e.g. "2021-05-28", or website page after the "/").""",height=250)
+    When possible, please cite source documents in your answer (calling the documents the transcript date, e.g. "2021-05-28", or website page after the "/").""", height=250)
 
-# Initialize ChatBot based on selected repository and temperature
-if "bot" not in st.session_state.keys() or st.session_state.custom_prompt != custom_prompt or st.session_state.selected_repo != selected_repo or st.session_state.temperature != temperature or "selected_retriever" not in st.session_state.keys() or st.session_state.selected_retriever != selected_retriever:
+# Initialize chat history if not present
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Hi, how can I help you today?"}]
+if "first_question" not in st.session_state:
+    st.session_state.first_question = ""
+
+# Initialize ChatBot instance if needed
+def initialize_bot():
     repo_id = repositories[selected_repo]
-    retriever_method = retriever_methods[selected_retriever]
-    bot = ChatBot(custom_template=custom_prompt, repo_id=repo_id, temperature=temperature, retriever_method=retriever_method)
+    bot = ChatBot(custom_template=custom_prompt, repo_id=repo_id, temperature=temperature)
     st.session_state.bot = bot
     st.session_state.custom_prompt = custom_prompt
     st.session_state.selected_repo = selected_repo
     st.session_state.temperature = temperature
-    st.session_state.retriever_method = retriever_method
-    st.session_state.messages = [{"role": "assistant", "content": "Hi, how can I help you today?"}]
     st.session_state.repo_id = repo_id
-    st.session_state.selected_retriever = selected_retriever
-    print("Initialized session_state.")
+
+if "bot" not in st.session_state or st.session_state.custom_prompt != custom_prompt or st.session_state.selected_repo != selected_repo or st.session_state.temperature != temperature:
+    initialize_bot()
 else:
     bot = st.session_state.bot
 
@@ -49,30 +48,25 @@ def generate_response(input):
     result = bot.rag_chain.invoke(input)
     return result
 
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "Hi, how can I help you today?"}]
-
 # Display chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
-        print(f"Displayed message: {message['content']}")
 
-# User-provided prompt
+# Handle user input
 if input := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": input})
     with st.chat_message("user"):
         st.write(input)
-        print(f"User input: {input}")
 
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Generating an answer.."):
-            response = generate_response(input) 
-            st.write(response)
-            print(f"Generated response: {response}")
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
-    print(f"Appended assistant response to session_state messages.")
+    # Store the first question if not already set
+    if not st.session_state.first_question:
+        st.session_state.first_question = input
+
+    # Generate response if needed
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Generating an answer.."):
+                response = generate_response(input) 
+                st.write(response)
+        st.session_state.messages.append({"role": "assistant", "content": response})
