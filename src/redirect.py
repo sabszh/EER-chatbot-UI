e3e9c415e5064ce944eff4f9c08a7bf98f0331e8
Sabrina Zaki Hansen <87586469+sabszh@users.noreply.github.com>
2024-06-14 14:19:51 +0200

    Update redirect.py

--- src/redirect.py
+++ src/redirect.py
@@ -3,6 +3,9 @@ import streamlit as st
 # Set the target URL
 target_url = "https://eerchat.ploomberapp.io/"
 
-# Redirect to the target URL
-st.write(f"Redirecting to the new [EER Chat]({target_url}) link on Ploomber...")
-st.markdown(f'<meta http-equiv="refresh" content="0; url={target_url}">', unsafe_allow_html=True)
+st.title("Redirect to the new link for EER Chat")
+
+# Create a button for redirection
+if st.button("Go to EER Chat"):
+    st.write(f"Redirecting to [EER Chat]({target_url})...")
+    st.markdown(f'<meta http-equiv="refresh" content="0; url={target_url}">', unsafe_allow_html=True)
