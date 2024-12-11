1f4d4b45c2423c2fc724233865b2266a998dd136
sabszh <sabrinazhansen@hotmail.com>
2024-12-11 09:33:17 +0100

    tutorial, updated code small changes

--- src/streamlit_rag_chatbot/main.py
+++ src/streamlit_rag_chatbot/main.py
@@ -1,49 +1,13 @@
-<<<<<<< Updated upstream:src/main.py
-from langchain_community.document_loaders import TextLoader, PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
-=======
 import os
 from datetime import datetime, timezone
 import logging
 from tenacity import retry, stop_after_attempt, wait_fixed
 from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint as HuggingFaceHub
->>>>>>> Stashed changes:src/streamlit_rag_chatbot/main.py
 from langchain_community.vectorstores.pinecone import Pinecone
-from langchain.prompts import PromptTemplate
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.schema.output_parser import StrOutputParser
-from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint as HuggingFaceHub
-from dotenv import load_dotenv
-import os
-from data_chunking import datachunk
 from pinecone import Pinecone as pc
-from pinecone import PodSpec
-from pinecone import ServerlessSpec
+import pinecone
 
-<<<<<<< Updated upstream:src/main.py
-load_dotenv()
-
-class ChatBot():
-    def __init__(self, custom_template=None, repo_id=None, temperature=0.8):
-        self.embeddings = HuggingFaceEmbeddings()
-        self.index_name = "boteer"
-        pinecone_instance = pc(api_key=os.getenv('PINECONE_API_KEY'), embeddings=self.embeddings)
-        spec = ServerlessSpec(cloud="aws",region="us-east-1")
-        
-        #if self.index_name not in pinecone_instance.list_indexes().names():
-        #    docs = datachunk()
-        #    pinecone_instance.create_index(name=self.index_name, metric="cosine", dimension=768, spec=spec)
-        #    self.docsearch = Pinecone.from_documents(docs, self.embeddings, index_name=self.index_name)
-        #    print("Created new Pinecone index and loaded documents")
-        #else:
-        #    self.docsearch = Pinecone.from_existing_index(self.index_name, self.embeddings)
-        #    print("Using existing Pinecone index")
-    
-        self.docsearch = Pinecone.from_existing_index(self.index_name, self.embeddings)
-=======
 # Set up logging for the chatbot
 logger = logging.getLogger(__name__)
 
@@ -75,53 +39,250 @@ class chatbot:
         self.embeddings = HuggingFaceEmbeddings()
         self.index_name = "eer-transcripts-pdfs"
         self.pinecone_instance = pc(api_key=os.getenv('PINECONE_API_KEY_2'), embeddings=self.embeddings)
->>>>>>> Stashed changes:src/streamlit_rag_chatbot/main.py
 
-        self.template = custom_template if custom_template else self.default_template()
+        # Self-assign parameters
+        self.user_name = user_name
+        self.session_id = session_id
         self.temperature = temperature
-        self.repo_id = repo_id
-        
+
+        # Instantiate the LLM
         self.llm = HuggingFaceHub(
-            repo_id=self.repo_id,
+            repo_id=os.getenv('repo_id'),
             temperature=temperature,
             top_p=0.8,
             top_k=50,
             huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_KEY')
         )
+        self.prompt_sourcedata = prompt_sourcedata
+        self.prompt_conv = prompt_conv
 
-        multiquery_retriever_llm = MultiQueryRetriever.from_llm(retriever=self.docsearch.as_retriever(), llm=self.llm)
-
-        # Initialize the chain
-        self.rag_chain = (
-            {"context": multiquery_retriever_llm.batch, "question": RunnablePassthrough()}
-            | PromptTemplate(template=self.template + self.template_end(), input_variables=["context", "question"])
-            | self.llm
-            | StrOutputParser()
-        )
-
-        print("Chain assembled...")
-
-    def default_template(self):
-        return """
-        You are a chatbot working for the Experimenting Experiencing Reflecting (EER) Project, a research endeavor investigating the connections between art and science.
-        You have access to a collection of documents, including descriptions of research activities, meeting transcripts, and other relevant materials.
-        Your main task is to help the user explore and reflect on the EER project.
-        All questions should pertain to the EER Project unless specified otherwise.
-        When possible, please cite source documents in your answer (calling the documents the transcript date, e.g. "2021-05-28", or website page after the "/").
+    def default_prompt_sourcedata(self, chat_history, original_data, user_input, user_name):
         """
-    
-    def template_end(self):
-        return """
-        Context: {context}
-        Question: {question}
-        Answer: 
+        Generates the default prompt for sourced data queries.
+
+        Args:
+            chat_history (str): The chat history so far.
+            original_data (str): The relevant data retrieved from the database.
+            user_input (str): The user's query.
+            user_name (str): The name of the user.
+
+        Returns:
+            str: The formatted prompt for sourced data queries.
         """
+        return f"""You are a chatbot assistant, working for the Experimenting Experiencing Reflecting (EER) Project, a research endeavor investigating the connections between art and science.
+    You have access to a collection of documents, including descriptions of research activities, meeting transcripts, and other relevant materials. Your main task is to help the user explore and reflect on the EER project. When possible, please cite source documents in your answer (calling the documents the transcript date, e.g. "2021-05-28", or website page after the "/"). 
+    You are now assisting the user "{user_name}" with their query: "{user_input}". Below is the relevant data from the EER group that was retrieved from the database for this query: "{original_data}". Based on this data, provide a concise answer to the user’s question in maximum 2 paragraphs. The previous chat history for this session so far is: {chat_history} Your response:"""
+
+    def default_prompt_conv(self, chat_history, user_input, llm_response, past_chat, user_name):
+        """
+        Generates the default prompt for conversation context queries.
+
+        Args:
+            chat_history (str): The chat history so far.
+            user_input (str): The user's query.
+            llm_response (str): The response from the LLM.
+            past_chat (str): Relevant data from past conversations.
+            user_name (str): The name of the user.
+
+        Returns:
+            str: The formatted prompt for conversation context queries.
+        """
+        return f"""You are an assistant observing conversations between the user and another LLM regarding meeting transcripts and content from the Experimenting Experiencing Reflecting (EER) Project, a research group investigating the connections between art and science.
+    All interactions between people and the LLM are recorded and stored in your database. When people ask questions about the data, you get the question and the answer from the LLM. You use that data to search your database of past conversations for conversations that might be related. You will create a summary of those past conversations no longer than 4 sentences. Your summary should mention the name of the person involved in the past conversations, so that if the user wants to, they can follow up with them.
+    Here is the last question asked by the user in this session: "{user_name}" asked: {user_input}
+    Here is what the LLM you are watching responded with: “{llm_response}”
+    Here is relevant data from past conversations that is relevant: {past_chat}
+    Here is the chat history for this session, so that your response can be aware of the context: {chat_history}
+    Your response: """
+
+    def retrieve_docs(self, input, index, excluded_session_id=None, k=5):
+        """
+        Retrieves documents from a Pinecone index, optionally excluding a specific session ID.
+
+        Args:
+            input (str): The input query for retrieving documents.
+            index (str): The name of the Pinecone index to retrieve documents from.
+            excluded_session_id (str, optional): Session ID to exclude from retrieval (for avoiding duplicate data).
+            k (int, optional): The number of documents to retrieve. Defaults to 5.
+
+        Returns:
+            list: A list of retrieved documents.
+        """
+        try:
+            docsearch = Pinecone.from_existing_index(index, self.embeddings)
+
+            if index == "eer-interaction-data":
+                search_kwargs = {
+                    "k": k,
+                    "filter": {
+                        "session_id": {"$ne": excluded_session_id}
+                    }
+                }
+            else:
+                search_kwargs = {"k": k}
+
+            retriever = docsearch.as_retriever(search_kwargs=search_kwargs)
+            docs = retriever.invoke(input)
+            return docs
+        except Exception as e:
+            logger.error("Error retrieving documents: %s", e)
+            raise
+
+    @retry(stop=stop_after_attempt(5), wait=wait_fixed(2))
+    def invoke_llm_with_retry(self, prompt):
+        """
+        Invokes the LLM with retry logic to handle transient connection issues.
+
+        Args:
+            prompt (str): The prompt to send to the LLM.
+
+        Returns:
+            str: The LLM's response to the prompt.
+        """
+        response = self.llm.invoke(prompt)
+        return response
+
+    def get_llm_response(self, prompt):
+        """
+        Generates a response from the LLM, retrying if necessary.
+
+        Args:
+            prompt (str): The prompt to send to the LLM.
+
+        Returns:
+            str: The LLM's response or an error message if the invocation fails.
+        """
+        try:
+            response = self.invoke_llm_with_retry(prompt)
+            return response
+        except Exception as e:
+            error = f"Error invoking LLM: {e}"
+            return error
+
+    def format_context(self, documents, chat=False):
+        """
+        Formats the context from retrieved documents for use in prompts.
+
+        Args:
+            documents (list): List of documents retrieved from Pinecone.
+            chat (bool, optional): Whether to format as chat context. Defaults to False.
+
+        Returns:
+            str: A formatted string representing the context.
+        """
+        context = ""
+
+        for idx, doc in enumerate(documents, start=1):
+            metadata = doc.metadata
+            if chat:
+                context += (
+                    f'User {idx}: {metadata.get("user_name", "Unknown User")}\n'
+                    f'Chat session {idx}: {metadata.get("session_id", "Unknown Session ID")}\n'
+                    f'User Question: "{metadata.get("user_question", "Unknown Question")}"\n'
+                    f'AI Response: "{metadata.get("ai_output", "Unknown Response")}"\n'
+                    f"Date: {metadata.get('date', 'Unknown Date')}\n\n"
+                )
+            else:
+                if metadata.get("page") is not None:
+                    context += (
+                        f"Document type: PDF {idx}\n"
+                        f"Page content: {doc.page_content}\n"
+                        f"Source: {metadata['source']}\n"
+                        f"Page: {metadata['page']}\n"
+                    )
+                else:
+                    context += (
+                        f"Document type: Meeting Transcript Exerpt {idx}\n"
+                        f"Person {idx}: {metadata.get('speaker_name', 'Unknown Speaker')}\n"
+                        f"Date: {metadata.get('date_time', 'Unknown Date')}\n"
+                        f"Content: {doc.page_content}\n\n"
+                    )
+
+        return context
+
+    def upsert_vectorstore(self, user_input, ai_output, user_name, session_id):
+        """
+        Upserts the conversation data to the Pinecone vector store, storing user input, AI output, and metadata.
+
+        Args:
+            user_input (str): The user's input.
+            ai_output (str): The AI's response.
+            user_name (str): The name of the user.
+            session_id (str): The unique session identifier.
+        """
+        try:
+            pinecone_instance_chat = pc(api_key=os.getenv('PINECONE_API_KEY'), embeddings=self.embeddings)
+            index_name = "eer-interaction-data"
+            environment = "gcp-starter"
+            index = pinecone_instance_chat.Index(index_name, environment=environment)
+
+            embedding = self.embeddings.embed_documents([user_input + ai_output])[0]
+
+            index.upsert(vectors=[
+                {
+                    'id': session_id,
+                    'values': embedding,
+                    'metadata': {
+                        "user_question": user_input,
+                        "ai_output": ai_output,
+                        "user_name": user_name,
+                        "session_id": session_id,
+                        "date": datetime.now(timezone.utc).isoformat(),
+                        "text": f"User input: {user_input}, AI output: {ai_output}"
+                    }
+                }
+            ])
+            logger.info("Upsert to Pinecone successful for session_id: %s", session_id)
+        except Exception as e:
+            logger.error("Error upserting to Pinecone: %s", e)
+            raise
+
+    def pipeline(self, user_input, user_name, session_id, chat_history=None):
+        """
+        Handles the full pipeline of receiving user input, generating responses, and storing data.
+
+        Args:
+            user_input (str): The user's input.
+            user_name (str): The name of the user.
+            session_id (str): The unique session identifier.
+            chat_history (str, optional): The chat history for context. Defaults to None.
+
+        Returns:
+            dict: A dictionary containing the AI output, source data, and past chat context.
+        """
+        if chat_history:
+            chat_history = chat_history + "\n\n"
+        else:
+            chat_history = ""
+
+        # Step 1: Retrieve source data
+        source_data = self.retrieve_docs(user_input, "eer-transcripts-pdfs")
+        formatted_source_data = self.format_context(source_data)
+
+        # Step 2: Generate LLM response from source data
+        sourcedata_response = self.get_llm_response(self.default_prompt_sourcedata(chat_history=chat_history, original_data=formatted_source_data, user_input=user_input, user_name=user_name))
+
+        # Step 3: Retrieve past chat context
+        past_chat_context = self.retrieve_docs(sourcedata_response, "eer-interaction-data", session_id)
+        formatted_chat_context = self.format_context(past_chat_context, chat=True)
+
+        # Step 4: Generate LLM response for conversation context, now considering combined chat history
+        conversation_response = self.get_llm_response(self.default_prompt_conv(chat_history=chat_history, user_input=user_input, llm_response=sourcedata_response, past_chat=formatted_chat_context, user_name=user_name))
+
+        # Step 5: Combine the responses
+        ai_output = f"{sourcedata_response}\n\n{conversation_response}"
+        logger.info(f"Pipeline generated response: {ai_output}")
+
+        # Step 6: Upsert to vector store
+        self.upsert_vectorstore(user_input, ai_output, user_name, session_id)
+
+        # Return a dictionary containing all relevant information
+        return {
+            "ai_output": ai_output,
+            "source_data": source_data,
+            "past_chat_context": past_chat_context
+        }
         
-<<<<<<< Updated upstream:src/main.py
-if __name__ == "__main__":
-    ChatBot(repo_id="mistralai/Mistral-7B-Instruct-v0.2")
-    print("Chatbot initialized...")
-=======
     @staticmethod
     def query_summaries(timestamp):
         # Set up pinecone client
@@ -147,4 +308,3 @@ if __name__ == "__main__":
             if match["metadata"]["date"] == timestamp:
                 return match["metadata"]
         return "No match found"
->>>>>>> Stashed changes:src/streamlit_rag_chatbot/main.py
--- src/streamlit_rag_chatbot/streamlit_app.py
+++ src/streamlit_rag_chatbot/streamlit_app.py
@@ -1,13 +1,4 @@
-from main import ChatBot
 import streamlit as st
-<<<<<<< Updated upstream:src/streamlit_app.py
-
-repositories = {
-    "Mistral-7B-Instruct-v0.2": "mistralai/Mistral-7B-Instruct-v0.2",
-    "Mixtral-8x7B-Instruct-v0.1": "mistralai/Mixtral-8x7B-Instruct-v0.1",
-    "Mistral-7B-Instruct-v0.1": "mistralai/Mistral-7B-Instruct-v0.1"
-}
-=======
 import logging  # Import logging
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 import uuid
@@ -24,22 +15,8 @@ logging.basicConfig(
     ]
 )
 logger = logging.getLogger(__name__)
->>>>>>> Stashed changes:src/streamlit_rag_chatbot/streamlit_app.py
 
-st.set_page_config(page_title="EER Chatbot")
-
-with st.sidebar:
-    st.title('EER Chatbot')
-    st.write("""Be aware, if you make any changes here that the chatbot will reload and your chat will be gone.
-             If you get an error, try a different model. If that does not work, it might be overloaded or down - so try again later.""")
-    selected_repo = st.selectbox("Select the Model Repository", list(repositories.keys()))
-    temperature = st.slider("Select the Temperature (0-2)", min_value=0.1, max_value=2.0, value=1.0, step=0.01)
-    custom_prompt = st.text_area('Edit Preprompt',
-    """You are a chatbot working for the Experimenting Experiencing Reflecting (EER) Project, a research endeavor investigating the connections between art and science.
-    You have access to a collection of documents, including descriptions of research activities, meeting transcripts, and other relevant materials.
-    Your main task is to help the user explore and reflect on the EER project.
-    All questions should pertain to the EER Project unless specified otherwise.
-    When possible, please cite source documents in your answer (calling the documents the transcript date, e.g. "2021-05-28", or website page after the "/").""", height=250)
+st.set_page_config(page_title="EER Transcript Explorer Bot", layout="wide")
 
 # Initialize chat history if not present
 if "messages" not in st.session_state:
@@ -47,50 +24,30 @@ if "messages" not in st.session_state:
 if "first_question" not in st.session_state:
     st.session_state.first_question = ""
 
-# Initialize ChatBot instance if needed
+# Functiion to initialize the chatbot
 def initialize_bot():
-    repo_id = repositories[selected_repo]
-    bot = ChatBot(custom_template=custom_prompt, repo_id=repo_id, temperature=temperature)
-    st.session_state.bot = bot
-    st.session_state.custom_prompt = custom_prompt
-    st.session_state.selected_repo = selected_repo
-    st.session_state.temperature = temperature
-    st.session_state.repo_id = repo_id
+    try:
+        if "bot" not in st.session_state or st.session_state.bot is None:
+            st.session_state.bot = chatbot()
+    except Exception as e:
+        st.error(f"Error initializing bot: {e}")
+        logger.error(f"Error initializing bot: {e}")
 
-if "bot" not in st.session_state or st.session_state.custom_prompt != custom_prompt or st.session_state.selected_repo != selected_repo or st.session_state.temperature != temperature:
-    initialize_bot()
-else:
-    bot = st.session_state.bot
+initialize_bot()
 
-# Function for generating LLM response
-def generate_response(input):
-    result = bot.rag_chain.invoke(input)
-    return result
+# Setting up the session state variables
+if "chat_data" not in st.session_state:
+    st.session_state.chat_data = []
 
-# Display chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+if "user_name" not in st.session_state:
+    st.session_state.user_name = None
 
-# Handle user input
-if input := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": input})
-    with st.chat_message("user"):
-        st.write(input)
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
 
-    # Store the first question if not already set
-    if not st.session_state.first_question:
-        st.session_state.first_question = input
+if "history" not in st.session_state:
+    st.session_state.history = StreamlitChatMessageHistory()
 
-<<<<<<< Updated upstream:src/streamlit_app.py
-    # Generate response if needed
-    if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
-            with st.spinner("Generating an answer.."):
-                response = generate_response(input) 
-                st.write(response)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-=======
 # Function to ask the user for their name
 @st.dialog("Please enter your name:", width="small")
 def ask_name():
@@ -279,4 +236,3 @@ if input_text:
     except Exception as e:
         st.error(f"Error generating response: {e}")
         logger.error(f"Error during input handling: {e}")
->>>>>>> Stashed changes:src/streamlit_rag_chatbot/streamlit_app.py
